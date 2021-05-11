3dffe755439c82add9ab2689278167e3e7bc5ff1
Daro <dario.palminio@gmail.com>
2021-05-11 11:31:59 -0400

    Add updateComponentData method in EmailClassComponent

--- src/components/EmailClassComponent.tsx
+++ src/components/EmailClassComponent.tsx
@@ -11,17 +11,23 @@ export default class EmailClassComponent extends React.Component<any, any> {
     super(props);
     this.state = { inputEmail: "", outputEmail: "" };
 
+    var self = this;
     //subscribe to store with callback function
     store.subscribe(() => {
       this._isMounted = true;
-      var value = store.getState().profileReducer.email;
       if (this._isMounted) {
-        this.setState({ outputEmail: value });
+        var data = store.getState().profileReducer.email;
+        self.updateComponentData(data);
+        console.log("Component receive data from store with subscribe");
         //This throws Warning: Can't call setState on a component that is not yet mounted. 
         //This is a no-op, but it might indicate a bug in your application. 
       }
-      console.log("Component receive data from store with subscribe");
+
     });
+  }
+
+  updateComponentData(data: string) {
+    this.setState({ outputEmail: data });
   }
 
   componentWillUnmount() {
@@ -34,7 +40,10 @@ export default class EmailClassComponent extends React.Component<any, any> {
 
   public handleEmailChange = () => {
     var value = this.state.inputEmail;
+
+    //Triggers an action for the reducer to modify the state in store
     store.dispatch(changeEmail({ email: value }));
+
     if (this.inputRef.current) this.inputRef.current.value = '';
   };
 
